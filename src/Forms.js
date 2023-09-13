497036cbcd96ab5e5698a85c633010b781e288a4
Ayush Manware <ayushmanware19@gmail.com>
2023-09-14 01:08:57 +0530

    css

--- src/Forms.js
+++ src/Forms.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
 class Form extends Component {
   constructor() {
@@ -42,59 +42,68 @@ class Form extends Component {
   render() {
     return (
       <>
-      <div className="formContainer">
-        <h1 id="heading" className="heading">Employee Feedback Form</h1>
-        <form>
-          <label id="font" htmlFor="name">
-            Name:{" "}
-          </label> <br />
-          <input
-            type="text"
-            name="name"
-            id="name"
-            onChange={this.handleChange}
-            value={this.state.name}
-          ></input>{" "}
-          <br />
-          <br />
-          <label id="font" htmlFor="Department">
-            Department:{" "}
-          </label><br />
-          <input
-            type="text"
-            name="Department"
-            id="Department"
-            onChange={this.handleChange}
-            value={this.state.Department}
-          ></input>
-          <br />
-          <br />
-          <label id="font" htmlFor="Ratings">
-            Rating
-          </label><br />
-          <input
-            type="number"
-            name="Rating"
-            id="Ratings"
-            onChange={this.handleChange}
-            value={this.state.Rating}
-          ></input>
-          <br />
-          <br />
-          <button onClick={this.handleSubmit} className="submitBtn">Submit</button>
-        </form>
+        <div className="formContainer">
+          <h1 id="heading" className="heading">
+            Employee Feedback Form
+          </h1>
+          <form>
+            <label id="font" htmlFor="name">
+              Name:{" "}
+            </label>{" "}
+            <br />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              onChange={this.handleChange}
+              value={this.state.name}
+            ></input>{" "}
+            <br />
+            <br />
+            <label id="font" htmlFor="Department">
+              Department:{" "}
+            </label>
+            <br />
+            <input
+              type="text"
+              name="Department"
+              id="Department"
+              onChange={this.handleChange}
+              value={this.state.Department}
+            ></input>
+            <br />
+            <br />
+            <label id="font" htmlFor="Ratings">
+              Rating
+            </label>
+            <br />
+            <input
+              type="number"
+              name="Rating"
+              id="Ratings"
+              onChange={this.handleChange}
+              value={this.state.Rating}
+            ></input>
+            <br />
+            <br />
+            <button onClick={this.handleSubmit} className="submitBtn">
+              Submit
+            </button>
+          </form>
         </div>
-        <div id="outercontainer">
-          {this.state.emptyArray.map((item, index) => {
-            return (
-              <div key={index} id="list" className="printContainer">
-                <h3 id="h1" className="print">
-                  Name: {item.name} || Department: {item.Department} || Rating:{" "}
-                  {item.Rating}
-                </h3>
-              </div>
-            );
-          })}
+        <div className="printMain">
+          <div id="outercontainer">
+            {this.state.emptyArray.map((item, index) => {
+              return (
+                <div key={index} id="list" className="printContainer">
+                  <h3 id="h1" className="print">
+                    Name: {item.name} || Department: {item.Department} ||
+                    Rating: {item.Rating}
+                  </h3>
+                 </div>
+              );
+            })}
+          </div>
         </div>
       </>
     );
